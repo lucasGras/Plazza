363ce460b6382765397b7afa4446e0ad15999fe1
ctinarelli <clement.tinarelli@epitech.eu>
2019-04-01 09:51:09 +0200

    [Add] Make cmakelist.

---
+++ Sources/Main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** plazza
+** File description:
+** main function
+*/
+
+#include <iostream>
+
+int main(int, const char *[])
+{
+	std::cout << "plazza" << std::endl;
+	return 0;
+}
