ba017da58a0a09bddb850a71c86c0c40d46f57b0
Adrien IACONO <adrien.iacono@epitech.eu>
2019-04-08 18:37:15 +0200

    [+] Adding KitcherData structure and tester for data shared

---
+++ Commons/Classes/KitchenData.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2018
+** KitchenData.hpp
+** File description:
+** Made by developers
+*/
+
+#ifndef PLAZZA_KITCHENDATA_HPP
+#define PLAZZA_KITCHENDATA_HPP
+
+struct KitchenData {
+	int avalaibleCooks;
+
+	int stockDoe;
+	int stockTomato;
+	int stockGruyere;
+	int stockSteak;
+	int stockMushrooms;
+	int stockHam;
+	int stockEggPlant;
+	int stockGoatCheese;
+	int stockChiefLove;
+};
+
+#endif //PLAZZA_KITCHENDATA_HPP
--- Kitchen/Sources/Kitchen.cpp
+++ Kitchen/Sources/Kitchen.cpp
@@ -5,8 +5,23 @@
 ** Made by developers
 */
 
+#include <iostream>
+#include <thread>
 #include "Kitchen.hpp"
+#include "Abstractions/SharedData.hpp"
+#include "KitchenData.hpp"
 
 int main(int ac, char **av) {
-	return (0);
+	plaz::abs::SharedData<KitchenData> sharedData("/kitchen01", O_CREAT | O_RDWR);
+	while (1) {
+		std::cout << "Avaiable Cooks: " << sharedData->avalaibleCooks << std::endl;
+		sharedData->avalaibleCooks++;
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	}
+	/*std::cout << "Starting listen DataQueue..." << std::endl;
+	plaz::abs::DataQueue queue("/kitchentest", O_CREAT | O_RDWR);
+	while (1) {
+		std::string response = queue.pull();
+		std::cout << "Received message: '" << response << "'" << std::endl;
+	}*/
 }
