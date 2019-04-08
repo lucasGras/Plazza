bebcb6de79e2e649bc279f7aa1b07e2784333e7b
Adrien IACONO <adrien.iacono@epitech.eu>
2019-04-08 17:52:58 +0200

    [~] Adding main in kitchen and fix makefile separator and modify CMakeList

--- Kitchen/Headers/Kitchen.hpp
+++ Kitchen/Headers/Kitchen.hpp
@@ -1,6 +1,9 @@
-//
-// Created by adrien2431 on 08/04/19.
-//
+/*
+** EPITECH PROJECT, 2018
+** Kitchen.hpp
+** File description:
+** Made by developers
+*/
 
 #ifndef PLAZZA_KITCHEN_HPP
 #define PLAZZA_KITCHEN_HPP
